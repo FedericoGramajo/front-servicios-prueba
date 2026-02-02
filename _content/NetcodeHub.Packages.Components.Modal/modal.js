98c942406f1916cb797475d9c9a38fa33b85387a
FedericoGramajo <54726454+FedericoGramajo@users.noreply.github.com>
2026-02-02 23:49:29 +0000

    Deploying to gh-pages from @ FedericoGramajo/front-servicios-prueba@f091b8a66a9a0cd100029456638ea76968c2e718 🚀

---
+++ _content/NetcodeHub.Packages.Components.Modal/modal.js
@@ -0,0 +1,23 @@
+﻿function bM(d, r) {
+    d.addEventListener("close", async e => {
+        await r.invokeMethodAsync("MA", d.returnValue);
+    });
+}
+function bOM(d) {
+    if (d && !d.open) {
+        d.showModal();
+    }
+}
+
+function bCM(d) {
+    if (d && d.open) {
+        d.close();
+    }
+}
+
+window.sD = function () {
+    document.getElementById('cD').showModal();
+}
+window.cD = function () {
+    document.getElementById('cD').close();
+}
